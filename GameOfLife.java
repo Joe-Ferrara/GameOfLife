01ec06e98ed7bfa1189c4dee9bfb2c19d4dc4eb3
Joe Ferrara <jferrara90@gmail.com>
2021-07-18 18:16:18 -0700

    Making game of life more interactive.
    Work in progress.

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,14 +1,28 @@
 //game_of_life.java = Conway's Game of Life
+
+import java.util.Scanner;
+
 class GameOfLife {
     public static void main(String[] args) throws java.io.IOException
     {
         //read in the size of the grid and create arrays
-        int size = 10; // fix at 10 for testing
+        int size, aliveCnt;
+        boolean playRandom;
         boolean[][] currentGeneration, nextGeneration;
-        currentGeneration = new boolean[size][size];
-        nextGeneration = new boolean[size][size];
-        readInitialGeneration(currentGeneration);
-
+        Scanner scan = new Scanner(System.in);
+        size = getSizeFromUser(scan);
+        aliveCnt = getAliveCntFromUser(scan, size);
+        playRandom = askPlayType(scan);
+        if (playRandom) {
+            currentGeneration = new boolean[size][size];
+            nextGeneration = new boolean[size][size];
+            makeRandomGrid(currentGeneration, aliveCnt);
+        }
+        else {
+            currentGeneration = new boolean[size][size];
+            nextGeneration = new boolean[size][size];
+            readInitialGeneration(currentGeneration);
+        }
         // read in the number of generations to simulate
         int cycles = 4; // fix at 4 for testing
         printState(currentGeneration);
@@ -21,6 +35,62 @@ class GameOfLife {
             nextGeneration = currentGeneration;
             currentGeneration = temp;
         }
+    }
+
+    static int makeRandomGrid(boolean[][] w, int aliveCnt) {
+        // need to determine best way to choose random alive cells
+        // make an array of length gridSize*gridSize
+        // with values 0, gridSize*gridSize - 1
+        // then shuffle the array and take the first aliveCnt elements
+        // then translate these numbers to positions in w
+        for (int i = 0; i < aliveCnt
+    }
+
+    static int getAliveCntFromUser(Scanner scan, int gridSize) {
+        boolean done = false;
+        int aliveCnt = 0;
+        while (! done) {
+            System.out.println("How many cells will start off alive?");
+            System.out.println("(type an integer and then press enter)");
+            aliveCnt = scan.nextInt();
+            int t = gridSize * gridSize + 1;
+            if (aliveCnt < t) done = true;
+            else {
+                System.out.println("You did not enter a valid number.");
+                System.out.printf("It must be less than %d.", t);
+            }
+        }
+        return aliveCnt;
+    }
+
+    static int getSizesFromUser(Scanner scan) {
+        int gridSize, aliveCnt;
+        System.out.println("Welcome to Game of Life!");
+        System.out.println("What size grid will you be playing with?");
+        System.out.println("(type an integer and then press enter)");
+        gridSize = scan.nextInt();
+        return gridSize;
+    }
+
+    static boolean askPlayType(Scanner scan) {
+        System.out.println("You can play in random mode or assign mode.");
+        System.out.println("In random mode the initial live cells are chosen");
+        System.out.println("at random.");
+        System.out.println("In assign mode you assign the initial live cells");
+        System.out.println("via coordinate ordered pairs, where (0,0) is the");
+        System.out.println("bottom left corner, the first coordinate moves");
+        System.out.println("horizontally and the second coordinate moves");
+        System.out.println("vertically.");
+        boolean done = false;
+        char c = 'a';
+        while (! done) {
+            System.out.println("Do you want to play in random mode? (y/n)");
+            c = scan.next().charAt(0);
+            if (c == 'y' || c == 'n') done = true;
+            else System.out.println("You did not give a valid response.");
+        }
+        if (c == 'y') return true;
+        else return false;
     }
 
     // read the initial generation from the input
@@ -91,6 +161,10 @@ class GameOfLife {
                     wNew[i][j] = EMPTY;
             }
         }
+    }
+
+    static int randomInteger(int l, int u) {
+        return l + (int)(Math.random() * ((u - l) + 1));
     }
 
     static final boolean ALIVE = true;
